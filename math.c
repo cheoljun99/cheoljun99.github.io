fe175628266577e7a2992c3795f562ba2b5a3c49
cheoljun99 <cheoljun99@naver.com>
2019-10-24 13:35:25 +0900

    first version

---
+++ math.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+ int num1, num2, num3;
+ printf("Enter 3 different integers (1-9):");
+ scanf("%d%d%d", &num1, &num2, &num3);
+ int a = num1 + num2 + num3;
+ printf("Sum is %d\n", a);
+
+ return 0;
+} 
+
