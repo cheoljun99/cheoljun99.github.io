37c638a3a0170be1bd1117bababa8de24f89a6f2
cheoljun99 <cheoljun99@naver.com>
2019-10-24 15:00:32 +0900

    fourth_version

--- math.c
+++ math.c
@@ -9,8 +9,15 @@ int main(void)
  printf("Sum is %d\n", a);
  int b = num1 * num2 * num3;
  printf("Product is %d\n", b);
+ int c = (num1 + num2 + num3) / 3;
+ printf("Average is %d\n", c);
+ if (num1 > num2 && num1 > num3)
+  printf("Largest is %d\n", num1);
+ else if (num2 > num1 && num2 > num3)
+  printf("Largest is %d\n", num2);
+ else if (num3 > num1 && num3 > num2)
+  printf("Largest is %d\n", num3);
 
  return 0;
-
 } 
 
